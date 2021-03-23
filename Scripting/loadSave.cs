f6230827b6f9ee491e7a594b4b514633d172fa68
Dan Smart <dan.j.smart@hotmail.co.uk>
2021-03-23 20:23:09 +0000

    Main Menu at reasonable level, beginning heavy code side

---
+++ Scripting/loadSave.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loadSave : MonoBehaviour
+{
+    public int levelComplete;
+    public int collectiblesFound;
+    public string level;
+
+    public int campaignKills;
+    public int endlessKills;
+    public int endlessDeaths;
+    public int endlessHits;
+    public int endlessScore;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Scripting/mainMenuScript.cs
+++ Scripting/mainMenuScript.cs
@@ -7,16 +7,28 @@ using UnityEngine.SceneManagement;
 public class mainMenuScript : MonoBehaviour
 {
     public Canvas mainMenuCanvas;
+    public Canvas newGameCanvas;
+    public Canvas loadGameCanvas;
+    public Canvas achievementCanvas;
+    public Canvas optionsCanvas;
     public AudioSource mainMenuMusic;
+
+    public Toggle easyToggle;
+    public Toggle normalToggle;
+    public Toggle hardToggle;
+    public string newGameText;
+
+    public Text newGameInfo;
 
     public void Start()
     {
-        Debug.Log("Test");
+        mainMenuCanvas.enabled = true;
+        newGameCanvas.enabled = false;
     }
 
     // Scene Changes
 
-    public void StartNewGame()
+    public void NewGameStart()
     {
         SceneManager.LoadScene("Pluto");
     }
@@ -34,9 +46,80 @@ public class mainMenuScript : MonoBehaviour
 
     //New Game Options/Panels
 
-    public void NewGameOptions()
+    public void NewGameButton()
     {
-
+        mainMenuCanvas.enabled = false;
+        newGameCanvas.enabled = true;
+        DifficultyInfo();
     }
 
+    public void NewGameBackButton()
+    {
+        mainMenuCanvas.enabled = true;
+        newGameCanvas.enabled = false;
+    }
+
+    public void DifficultyInfo()
+    {
+        if (easyToggle.isOn)
+        {
+            newGameText = "For players new to 2D Shoot 'em up. Enemies have 50% less health";
+        }
+        else if (normalToggle.isOn)
+        {
+            newGameText = "The optimal experience. No game modifiers.";
+        }
+        else if (hardToggle.isOn)
+        {
+            newGameText = "For people who like a challenge. Enemies have 50% more health. Hazards kill on impact.";
+        }
+        DisplayNewGameText();
+    }
+
+    public void DisplayNewGameText()
+    {
+        newGameInfo.text = newGameText;
+    }
+
+
+    // Load Game Options/Panels
+    public void LoadGameButton()
+    {
+        mainMenuCanvas.enabled = false;
+        loadGameCanvas.enabled = true;
+    }
+
+    public void LoadGameBackButton()
+    {
+        mainMenuCanvas.enabled = true;
+        loadGameCanvas.enabled = false;
+    }
+
+    // Achievement Panel
+
+    public void AchieveButton()
+    {
+        mainMenuCanvas.enabled = false;
+        achievementCanvas.enabled = true;
+    }
+
+    public void AchieveBackButton()
+    {
+        mainMenuCanvas.enabled = true;
+        achievementCanvas.enabled = false;
+    }
+
+    //Options Panel
+
+    public void OptionsButton()
+    {
+        mainMenuCanvas.enabled = false;
+        optionsCanvas.enabled = true;
+    }
+
+    public void OptionsBackButton()
+    {
+        mainMenuCanvas.enabled = true;
+        optionsCanvas.enabled = false;
+    }
 }
